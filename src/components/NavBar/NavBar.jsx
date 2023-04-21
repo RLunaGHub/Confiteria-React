14b07b48c1a158e857740295de89c883c2d4eb63
Ramiro Luna <ram_rsl@yahoo.com.ar>
2023-04-20 22:52:18 -0300

    PreEntrega1Luna

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,17 +2,31 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+
+
+
+
 import { CartWidget } from "../CartWidget/CartWidget"
+import logo from "./assets/logo.jpg"
+
+const stylesLogo = {
+    img: {
+        height: 35
+    }
+   
+}
+
 export const NavBar  = () => {
     return (
         <header>
     <Navbar bg="dark" variant="dark">
     <Container>
-      <Navbar.Brand href="#home">Confitería React</Navbar.Brand>
+       <img src={logo} alt="Imagen de logo forma de torta" style={stylesLogo.img} />
+        <Navbar.Brand href="#home">Confitería React</Navbar.Brand>
       <Nav className="me-auto">
         <Nav.Link href="#">Home</Nav.Link>
         <Nav.Link href="#">Productos</Nav.Link>
-        <Nav.Link href="#pricing">Contacto</Nav.Link>
+        <Nav.Link href="#">Contacto</Nav.Link>
       </Nav>
       <CartWidget />
     </Container>
