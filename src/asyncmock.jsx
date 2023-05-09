03bdd074e3d5b1fbbede66472e288751845dc9c7
Ramiro Luna <ram_rsl@yahoo.com.ar>
2023-05-09 19:36:11 -0300

    correcion detalle

--- src/asyncmock.jsx
+++ src/asyncmock.jsx
@@ -155,7 +155,7 @@ export const getProducts = () => {
 export const getProductById = (productId) => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(products.find((prod) => prod.id === productId));
+      resolve(products.find((prod) => prod.id === parseInt(productId)));
     }, 500);
   });
 };
